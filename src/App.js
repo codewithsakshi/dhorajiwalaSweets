a75c778e1f59ab9901f0c9ac17780a0b5a12c3b5
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-11-07 18:07:02 +0530

    add header section in the website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './component/header/Header';
 
 function App() {
   return (
     <div className="App">
-     <h1>Dhorajiwala Sweets</h1>
+      <Header/>
     </div>
   );
 }
---
+++ src/component/header/Header.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import './header.css'
+
+export default function Header() {
+    const [isVisible, setIsVisible] = useState(false);
+    const handleClick = () => {
+        setIsVisible(!isVisible);
+    };
+
+    return (
+        <div className="header-container">
+            <div className="contact-icon"><i class="fas fa-phone"></i>
+            <h3 className="phone-no">98222-21372</h3>
+            </div>
+            <img src="/dhorajiwala-logo" alt="dhorajiwala logo" className="dhorajiwala-logo"/>
+            <div className="icons-container">
+            <div className={`search ${isVisible === true ? 'active' : ''}`}>
+                <input type='text' className='input' placeholder='search' />
+                <button className='btn' onClick={handleClick}>
+                    <i className='fas fa-search'></i>
+                </button>
+            </div>
+            <i class="fas fa-user"></i>
+            <i class="fas fa-shopping-cart"></i>
+            </div>
+        </div>
+    )
+}
+
+// sakshimittal3005@gmail.com
